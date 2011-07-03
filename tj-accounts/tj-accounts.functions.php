64e0c5323123d24afbd2ac14d7b2eea89cfb93eb
Joe Hoyle <joehoyle@gmail.com>
2011-07-03 15:15:22 +0100

    use bloginfo( 'login_url' ) in tja_get_login_url()

--- tj-accounts/tj-accounts.functions.php
+++ tj-accounts/tj-accounts.functions.php
@@ -675,7 +675,7 @@ function tja_get_profile_user() {
  * @return string
  */
 function tja_get_login_url( $redirect = null, $message = null ) {
-	$url = trailingslashit( get_bloginfo( 'url' ) ) . 'login/';
+	$url = get_bloginfo( 'login_url', 'display' );
 
 	if( $redirect )
 		$url = add_query_arg( 'redirect_to', urlencode($redirect), $url );
