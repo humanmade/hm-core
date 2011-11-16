4d55de873f11c6af1b4b9b3d83fe93d43dfa00c5
Tom Willmot <tom@humanmade.co.uk>
2011-11-16 15:22:09 +0000

    Updated comments.

--- hm-core.plugin.php
+++ hm-core.plugin.php
@@ -11,6 +11,7 @@ Author URI: http://hmn.md/
 // TODO We should use theme_supports for the loading
 
 // Don't load if there is another version loaded
+// TODO we shouldn't even activate
 if ( defined( 'HM_CORE_PATH' ) || defined( 'HELPERPATH' ) )
 	return;
 
@@ -21,31 +22,30 @@ define( 'HM_CORE_URL', str_replace( ABSPATH, site_url( '/' ), HM_CORE_PATH ) );
 // Load core files
 include_once( HM_CORE_PATH . 'hm-core.debug.php' );
 include_once( HM_CORE_PATH . 'hm-core.functions.php' );
+include_once( HM_CORE_PATH . 'hm-core.rewrite.php' );
 include_once( HM_CORE_PATH . 'hm-core.messages.php' );
 
 // Related posts function
+// @todo wrap in theme-supports?
 include_once( HM_CORE_PATH . 'hm-core.related-posts.php' );
 
-// Load the custom media button support
+// Load the custom media button support unless it's specifically disabled
 if ( ! defined( 'HM_ENABLE_MEDIA_UPLOAD_EXTENSIONS' ) || HM_ENABLE_MEDIA_UPLOAD_EXTENSIONS )
     include_once( HM_CORE_PATH . 'media-uploader.extensions.php' );
 
-// Load WPThumb
+// Load WPThumb unless it's specifically disabled or already included elsewhere
 if ( ( ! defined( 'HM_ENABLE_PHPTHUMB' ) || HM_ENABLE_PHPTHUMB ) && ! function_exists( 'wpthumb' ) )
     include_once( HM_CORE_PATH . 'WPThumb/wpthumb.php' );
 
-// Load the accounts module
+// Only load HM Accounts if it's enabled
 if ( defined( 'HM_ENABLE_ACCOUNTS' ) && HM_ENABLE_ACCOUNTS !== false )
     include_once( HM_CORE_PATH . 'hm-accounts/hm-accounts.php' );
 
-// Load the custom rewrite rules
-include_once( HM_CORE_PATH . 'hm-core.rewrite.php' );
-
-// Load the Paypal class
+// Load the Paypal class if enabled
 if ( defined( 'HM_ENABLE_PAYPAL' ) && HM_ENABLE_PAYPAL !== false )
     include_once( HM_CORE_PATH . 'paypal/paypal.functions.php' );
 
-// Load the js functions
+// Load the js functions unless specifically disabled
 if ( ! defined( 'HM_ENABLE_SCRIPTS' ) || HM_ENABLE_SCRIPTS ) :
 
     // Include the js script shortcode
