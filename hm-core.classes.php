05815fe6b60c31983cadccea56cdde6610c30b71
Joe Hoyle <joehoyle@gmail.com>
2011-12-22 11:05:53 +0000

    Fixed HM_Feature_Image_Meta_Box show added info on set post thumbnail via ajax

--- hm-core.classes.php
+++ hm-core.classes.php
@@ -7,19 +7,23 @@ class HM_Feature_Image_Meta_Box {
 	public $show_watermark_on_thumbnail;
 	private $post;
 	
+	function __construct( $show_view_large_link = false, $link_image_to_gallery_tab = false, $show_watermark_on_thumbnail = false ) {
+		
+		$this->show_view_large_link = $show_view_large_link;
+		$this->link_image_to_gallery_tab = $link_image_to_gallery_tab;
+		$this->show_watermark_on_thumbnail = $show_watermark_on_thumbnail;
+		
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX && isset( $_POST['action'] ) && $_POST['action'] == 'set-post-thumbnail' ) {
+			$this->post = get_post( $_POST['post_id'] );
+			$this->setup_hooks();
+		}
+		
+	}
+	
 	public function display( $post ) {
 		
 		$this->post = $post;
-
-    	if ( $this->show_watermark_on_thumbnail )
-    		add_filter( 'image_downsize', array( $this, '_show_watermark_on_image_downsize' ), 99, 3 );
-    	
-    	if ( $this->link_image_to_gallery_tab )
-    		add_filter( 'admin_post_thumbnail_html', array( $this, '_set_featured_image_link_to_gallery_tab' ) );
-    		
-    	
-    	if ( $this->show_view_large_link )
-			add_filter( 'admin_post_thumbnail_html', array( $this, '_admin_post_thumbnail_html' ) );
+		$this->setup_hooks();
 		
 		post_thumbnail_meta_box( $post );
 	
@@ -58,8 +62,24 @@ class HM_Feature_Image_Meta_Box {
     
 	}
 	
+	private function setup_hooks() {
+	
+		if ( $this->show_watermark_on_thumbnail )
+    		add_filter( 'image_downsize', array( $this, '_show_watermark_on_image_downsize' ), 99, 3 );
+    	
+    	if ( $this->link_image_to_gallery_tab )
+    		add_filter( 'admin_post_thumbnail_html', array( $this, '_set_featured_image_link_to_gallery_tab' ) );
+    	    	
+    	if ( $this->show_view_large_link )
+			add_filter( 'admin_post_thumbnail_html', array( $this, '_admin_post_thumbnail_html' ) );
+	
+	}
+	
 	private function get_view_large_link() {
-		return '<a href="' . reset( wp_get_attachment_image_src( get_post_thumbnail_id( $this->post->ID ), 'width=800&height=800&crop=0' ) ) . '" target="_blank">View Large</a>';	
+		
+		if ( get_post_thumbnail_id( $this->post->ID ) )
+			return '<a href="' . reset( wp_get_attachment_image_src( get_post_thumbnail_id( $this->post->ID ), 'width=800&height=800&crop=0' ) ) . '" target="_blank">View Large</a>';	
+		
 	}
 
 }
