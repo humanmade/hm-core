6a5aadc480f130b2cd20dc42d3afdef86a21645a
Tom Willmot <tom@humanmade.co.uk>
2011-12-28 15:05:04 +0000

    New unregister_post_type function.

--- hm-core.functions.php
+++ hm-core.functions.php
@@ -1725,3 +1725,21 @@ function hma_class( $classes, $bool ) {
 	<?php }
 
 }
+
+
+if ( ! function_exists( 'unregister_post_type' ) ) :
+
+function unregister_post_type( $post_type ) {
+
+	global $wp_post_types;
+
+	if ( isset( $wp_post_types[ $post_type ] ) ) {
+		unset( $wp_post_types[ $post_type ] );
+		return true;
+	}
+
+	return false;
+
+}
+
+endif;
