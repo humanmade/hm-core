6086b8f54b41298fa3e4e6371ca9c6dc7d825aa3
Joe Hoyle <joehoyle@gmail.com>
2012-06-07 17:34:33 +0100

    Added hook for login error message

--- hm-accounts/hm-accounts.classes.php
+++ hm-accounts/hm-accounts.classes.php
@@ -325,7 +325,14 @@ add_action( 'init', function() {
 			$status = $hm_accounts->login( $details );
 
 			if ( is_wp_error( $status ) )
-				hm_error_message( $status->get_error_message() ? $status->get_error_message() : 'Something went wrong, error code: ' . $status->get_error_code(), 'login' );
+				hm_error_message( 
+					apply_filters( 
+						'hma_login_error_message', 
+						$status->get_error_message() ? $status->get_error_message() : 'Something went wrong, error code: ' . $status->get_error_code(), 
+						$status
+					), 
+					'login' 
+				);
 
 
 			hma_do_login_redirect( $status, true );
