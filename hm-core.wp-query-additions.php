a28c25e98856fdc24b8c362b627708ae32bdeacb
K. Adam White <kadamwhite@users.noreply.github.com>
2024-01-05 11:16:03 -0500

    Guard against non-string argument to explode()
    Fixes fatal error,
    Uncaught TypeError: explode(): Argument #2 ($string) must be of type string, array given
    in /usr/src/app/content/mu-plugins/hm-core/hm-core.wp-query-additions.php:129

--- hm-core.wp-query-additions.php
+++ hm-core.wp-query-additions.php
@@ -126,7 +126,13 @@ function hm_allow_any_orderby_to_wp_query( $orderby, $wp_query ) {
 
 	global $wpdb;
 	$query = wp_parse_args( $wp_query->query );
-	$orders = explode( ' ', isset( $query['orderby'] ) ? $query['orderby'] : '' );
+	if ( is_array( $query['orderby'] ?? null ) ) {
+		$orders = $query['orderby'];
+	} elseif ( is_string( $query['orderby'] ?? null ) ) {
+		$orders = explode( ' ', $query['orderby'] );
+	} else {
+		$orders = [];
+	}
 
 	if( count( $orders ) <= 1  )
 		return $orderby;
